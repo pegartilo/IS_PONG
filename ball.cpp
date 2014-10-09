13d9dbcd4de6ae800258a4b8a643ea864388923d
blorente <blorente@ucm.es>
2014-10-09 18:33:45 +0200

    Commit para añadir lo de Pedro

---
+++ ball.cpp
@@ -0,0 +1,180 @@
+#include "ball.h"
+
+
+void updateBall(tBall &ball, bool collision_player, int collisionZonePlayer, bool collision_wall)//esta funcion primero mira si es necesario cambiar la direccion por una collision tanto con un jugador como con un muro
+{
+	updateBallDirection(ball, collision_player, collisionZonePlayer, collision_wall);
+	updateBallPosition(ball);
+}
+
+void updateBallPosition(tBall &ball)
+{
+	switch (ball.direction)
+	{
+		case up_right:
+		{
+			ball.position.x += 1;
+			ball.position.y += 1;
+			break;
+		}
+		case mid_right:
+		{
+			ball.position.x += 1;
+			break;
+		}
+		case down_right:
+		{
+			ball.position.x += 1;
+			ball.position.y -= 1;
+			break;
+		}
+		case up_left:
+		{
+			ball.position.x -= 1;
+			ball.position.y += 1;
+			break;
+		}
+		case mid_left:
+		{
+			ball.position.x -= 1;
+			break;
+		}
+		case down_left:
+		{
+			ball.position.x -= 1;
+			ball.position.y -= 1;
+			break;
+		}
+	}
+}
+
+void updateBallDirection(tBall &ball, bool collision_player, int collisionZonePlayer, bool collision_wall)
+{
+	if (collision_player && collision_wall)
+	{
+		if ((ball.direction == up_right) && (collisionZonePlayer == 1))
+		{
+			ball.direction = down_left;
+		}
+		else if ((ball.direction == up_left) && (collisionZonePlayer == 1))
+		{
+			ball.direction = down_right;
+		}
+		else if ((ball.direction == down_right) && (collisionZonePlayer == 3))
+		{
+			ball.direction = up_left;
+		}
+		else if ((ball.direction == down_left) && (collisionZonePlayer == 3))
+		{
+			ball.direction = up_right;
+		}
+	}
+	else if (!collision_wall && collision_player)
+	{
+		if (collisionZonePlayer == 2)
+		{
+			if (ball.direction == up_left)
+			{
+				ball.direction = up_right;
+			}
+			else if (ball.direction == mid_left)
+			{
+				ball.direction = mid_right;
+			}
+			else if (ball.direction == down_left)
+			{
+				ball.direction = down_right;
+			}
+			//*****************************
+			if (ball.direction == up_right)
+			{
+				ball.direction = up_left;
+			}
+			else if (ball.direction == mid_right)
+			{
+				ball.direction = mid_left;
+			}
+			else if (ball.direction == down_right)
+			{
+				ball.direction = down_left;
+			}
+		}
+		//**************************************************
+		if (collisionZonePlayer == 1)
+		{
+			if (ball.direction == up_left)
+			{
+				ball.direction = up_right;
+			}
+			else if (ball.direction == mid_left)
+			{
+				ball.direction = up_right;
+			}
+			else if (ball.direction == down_left)
+			{
+				ball.direction = mid_right;
+			}
+			//*****************************
+			if (ball.direction == up_right)
+			{
+				ball.direction = up_left;
+			}
+			else if (ball.direction == mid_right)
+			{
+				ball.direction = up_left;
+			}
+			else if (ball.direction == down_right)
+			{
+				ball.direction = mid_left;
+			}
+		}
+		//**************************************************
+		if (collisionZonePlayer == 3)
+		{
+			if (ball.direction == up_left)
+			{
+				ball.direction = mid_right;
+			}
+			else if (ball.direction == mid_left)
+			{
+				ball.direction = down_right;
+			}
+			else if (ball.direction == down_left)
+			{
+				ball.direction = down_right;
+			}
+			//*****************************
+			if (ball.direction == up_right)
+			{
+				ball.direction = mid_left;
+			}
+			else if (ball.direction == mid_right)
+			{
+				ball.direction = down_left;
+			}
+			else if (ball.direction == down_right)
+			{
+				ball.direction = down_left;
+			}
+		}
+	}
+	else if (collision_wall && !collision_player)
+	{
+		if (ball.direction == up_right)
+		{
+			ball.direction = down_right;
+		}
+		else if (ball.direction == up_left)
+		{
+			ball.direction = down_left;
+		}
+		else if (ball.direction == down_right)
+		{
+			ball.direction = up_right;
+		}
+		else if (ball.direction == down_left)
+		{
+			ball.direction = up_left;
+		}
+	}
+}
