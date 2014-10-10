cf83ec856e3f6a3e97d67789a6639af784ba68dd
Mortadelegle <Mortadelegle@users.noreply.github.com>
2014-10-10 12:16:04 +0200

    Create Meta.cpp

---
+++ Meta.cpp
@@ -0,0 +1,33 @@
+#include "Meta.h"
+
+void initializeScore(tScore &score)
+{
+	score.player1 = score.player2 = 0;
+}
+
+bool updateScore(tScore &score, int winner)
+{
+	bool won = false;
+
+	if (winner == 1) {
+		score.player1++;
+	}
+	else {
+		score.player2++;
+	}
+
+	// Check if one player win's
+
+	if ((score.player1 == MAX_ROUNDS - 1) || (score.player1 == MAX_ROUNDS - 1)) {
+		won = true;
+	}
+
+	return won;
+}
+
+void displayScore(tScore score)
+{
+	cout << setw(20) << " " << "---------------------" << endl; // Top lines of the score
+	cout << setw(20) << " " << "-------" << score.player1 << "-" << score.player2 << "-------" << endl;
+	cout << setw(20) << " " << "---------------------" << endl; // Bottom lines of the score
+}
