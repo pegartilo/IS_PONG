6e84d4f24ccf96b39f2c8296451a4506a9b663ec
Mortadelegle <Mortadelegle@users.noreply.github.com>
2014-10-14 20:18:13 +0200

    First working demo!

--- Ball.h
+++ Ball.h
@@ -3,20 +3,18 @@
 
 typedef enum {up_right, mid_right, down_right, up_left, mid_left, down_left} tDir;// con esto contemplamos las 6 direcciones posibles mas facil que hacer simetria.
 
-typedef struct 
-{
+typedef struct {
 	int x;
 	int y;
-}tBallPosition;
+} tBallPosition;
 
- typedef struct 
-{
+ typedef struct {
 	tBallPosition position;
 	tDir direction;
- }tBall;
+ } tBall;
 
-void updateBall(tBall &ball, bool collision_player, int collisionZonePlayer, bool collision_wall);//LLama primero a updateBallDirection para saber cual es su direccion actul y despues a updateBallPosition
-void updateBallPosition(tBall &ball);
-void updateBallDirection(tBall &ball, bool collision_player, int collisionZonePlayer, bool collision_wall);// collisionZonePlayer es 1 si es arriba 2 en el medio y 3 abajo 
+void updateBall (tBall &ball, bool collision_player, int collisionZonePlayer, bool collision_wall);//LLama primero a updateBallDirection para saber cual es su direccion actul y despues a updateBallPosition
+void updateBallPosition (tBall &ball);
+void updateBallDirection (tBall &ball, bool collision_player, int collisionZonePlayer, bool collision_wall);// collisionZonePlayer es 1 si es arriba 2 en el medio y 3 abajo 
 
 #endif
